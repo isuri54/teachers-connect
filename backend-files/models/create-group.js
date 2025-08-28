712c8f5333b6f7d37d0b606756fed6475b81a7f3
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-08-28 18:18:02 +0530

    added create-group file

---
+++ backend-files/models/create-group.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const CreateGroupSchema = new mongoose.Schema ({
+    name: { type: String, required: true },
+    subject: { type: String, required: true },
+    grade: { type: String, default: "No Grade" },
+    description: { type: String },
+    coverPhoto: { type: String },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+}, { timestamps: true });
+
+export default mongoose.model("Create-Group", CreateGroupSchema);
