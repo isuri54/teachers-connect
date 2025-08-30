1f9baf4725569c8d82e6068044fc234bff473867
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-08-30 22:57:22 +0530

    update user.js

--- backend-files/models/user.js
+++ backend-files/models/user.js
@@ -4,7 +4,9 @@ const UserSchema = new mongoose.Schema ({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["school teacher","tution teacher"], default: "school teacher"] },
+    school: { type: String },
+    subject: { type: String, required: true },
+    profilePicture: { type: String, default: "" },
     createdAt: { type: String, default: Date.now },
 });
 
