e4776f8b0bf33212dbf2ef971ea15ebd7916b286
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-08-30 22:58:07 +0530

    add userRoutes.js

---
+++ backend-files/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import user from "../models/user";
+
+const router = express.Router();
+
+router.get("/:id", async (req, res) => {
+    try {
+        const { name, subject, school, profilePicture } = req.body;
+        const User = await user.findByIdAndUpdate(
+            req.params.id,
+            { name, subject, school, profilePicture },
+            { new: true }
+        );
+        res.json(User);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+export default router;
